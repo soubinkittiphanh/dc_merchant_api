6ae9e208e629985831077951d9f842dde6d74fe6
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2023-06-11 17:51:53 +0700

    jack tapi

--- src/config/env.js
+++ src/config/env.js
@@ -2,8 +2,8 @@ const config={
     port:process.env.PORT||8080,
     nodeEnv:process.env.NODE_ENV||"development",
     host:process.env.HOST||"localhost",
-    actksecret:'e3848b9bd2e3eee522325953aafc118ed017c811cc93fae99a4b2f5ba3506e0e217636b3b509055900cb1da7594b0ce6c7192907213291818a4fdc89bf605ce8',
-    rfTkSecret:'e3848b9bd2e3eee522325953aafc118ed017c811cc93fae99a4b2f5ba3506e0e217636b3b509055900cb1da7',
+    actksecret:'Jacke3848b9bd2e3eee522325953aafc118ed017c811cc93fae99a4b2f5ba3506e0e217636b3b509055900cb1da7594b0ce6c7192907213291818a4fdc89bf605ce8',
+    rfTkSecret:'Jacke3848b9bd2e3eee522325953aafc118ed017c811cc93fae99a4b2f5ba3506e0e217636b3b509055900cb1da7',
     db:{
         host:'209.209.40.80',
         user:'soubin',
@@ -12,8 +12,8 @@ const config={
         // password:'sdat@3480',
         // database:'peeair_demo',
         // database:'dcommerce_uat',
-        // database:'dcommerce_jack42_production',
-        database:'dcommerce_production',
+        database:'dcommerce_jack42_production',
+        // database:'dcommerce_production',
         port: 34248,
     }
 }
